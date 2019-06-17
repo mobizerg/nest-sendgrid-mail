8b4d49604fc3db922066e559a0613ae84b245502
Frozenex <arun.frozenex@gmail.com>
2019-06-17 17:44:16 +0530

    Removed loadash

--- lib/sendgrid-mail.helper.ts
+++ lib/sendgrid-mail.helper.ts
@@ -1,5 +1,3 @@
-import { upperCase } from 'lodash';
-
 export function getMailerToken(name?: string): string {
-  return name ? `SENDGRID_MAILER_${upperCase(name)}` : 'SENDGRID_MAILER_DEFAULT';
+  return name ? `SENDGRID_MAILER_${name.toUpperCase()}` : 'SENDGRID_MAILER_DEFAULT';
 }
